376dd544bcf11bb15e2c0935b5d78ae407eea376
shuishuipingan <zhang2013148586@163.com>
2026-09-11 02:29:55 +0800

    test: provide web server in emitted plugin verifier

--- plugins/inkweaver-dsh/scripts/verify-built.mjs
+++ plugins/inkweaver-dsh/scripts/verify-built.mjs
@@ -65,6 +65,7 @@ host.provide('connection', {
 })
 host.provide('workspaceRegistry', { get: () => undefined })
 host.provide('settings', { register: () => ({}) })
+host.provide('webServer', { register: () => async () => {} })
 await host.plugin(Loader)
 host.loader.builtins.include = Include
 const builtModule = await import(pathToFileURL(join(root, manifest.main)).href)
